affc069d6b2bae3a11642fb9cbd6967e1dbd1f72
home <home@LAP-WOJCIC-R2.titian.co.uk>
2018-09-02 14:40:22 +0100

    asking for settings works

--- FCompare/__init__.py
+++ FCompare/__init__.py
@@ -1,42 +1,55 @@
-from fman import DirectoryPaneCommand, show_alert, load_json, OK, CANCEL
+from fman import DirectoryPaneCommand, show_alert, load_json, OK, CANCEL, DirectoryPane
 from fman.url import as_human_readable
 import subprocess
 from .settings import Settings
 
+# TODO it would be good to display it in the "CompareWithSaved" command in the command palete
+"""Currently selcted file to compare"""
 savedToCompare = ""
 settings = Settings()
 
-class SaveToCompare(DirectoryPaneCommand):	
-	def __call__(self):
-		global savedToCompare
-		files = self.get_chosen_files()
-		selectedFile = files[0]
-		savedToCompare = as_human_readable(selectedFile)
+class SaveToCompare(DirectoryPaneCommand):
+    def __call__(self):
+        global savedToCompare
+        files = self.get_chosen_files()
+        selectedFile = files[0]
+        savedToCompare = as_human_readable(selectedFile)
 
-	def is_visible(self):
-		files = self.get_chosen_files()
-		return len(files) == 1
+    def is_visible(self):
+        files = self.get_chosen_files()
+        return len(files) == 1
+
 
 class CompareWithSaved(DirectoryPaneCommand):
-	def __call__(self):
+    def __call__(self):
+        global savedToCompare
+        selectedFile = as_human_readable(self.get_chosen_files()[0])
+        ComparisonToolRunner.compare_files(savedToCompare, selectedFile)
+        pass
 
-		global savedToCompare
-		selectedFile = as_human_readable(self.get_chosen_files()[0])
-		subprocess.call([settings.get_comparison_tool(), savedToCompare, selectedFile])
-		pass
-
-	def is_visible(self):
-		global savedToCompare
-		return savedToCompare != "" and len(self.get_chosen_files()) == 1
-
+    def is_visible(self):
+        global savedToCompare
+        return savedToCompare != "" and len(self.get_chosen_files()) == 1
 
 class CompareFiles(DirectoryPaneCommand):
-	def __call__(self):
-		global savedToCompare
-		selectedFile1 = as_human_readable(self.get_chosen_files()[0])
-		selectedFile2 = as_human_readable(self.get_chosen_files()[1])
-		subprocess.call([settings.get_comparison_tool(), selectedFile1, selectedFile2])
-		pass
+    def __call__(self):
+        selectedFile1 = as_human_readable(self.get_chosen_files()[0])
+        selectedFile2 = as_human_readable(self.get_chosen_files()[1])
+        ComparisonToolRunner.compare_files(selectedFile1, selectedFile2)
+        pass
 
-	def is_visible(self):
-		return len(self.get_chosen_files()) == 2
+    def is_visible(self):
+        return len(self.get_chosen_files()) == 2
+
+class ComparisonToolRunner:
+    @staticmethod
+    def compare_files(lhsFile: str, rhsFile: str):
+        global settings
+        comparisonTool = settings.get_comparison_tool()
+        if comparisonTool:
+            subprocess.call([comparisonTool, lhsFile, rhsFile])
+
+def _ifnull(var, val):
+  if var is None:
+    return val
+  return var
--- FCompare/settings.py
+++ FCompare/settings.py
@@ -1,11 +1,13 @@
-from fman import show_alert, OK, CANCEL, show_file_open_dialog, PLATFORM, load_json, save_json, PLATFORM
+from fman import show_alert, OK, CANCEL, show_file_open_dialog, PLATFORM, \
+ load_json, save_json, PLATFORM
 from subprocess import Popen
 from core.os_ import get_popen_kwargs_for_opening
 from os.path import exists, splitdrive
 import os
 import sys
+import json
 
-_COMPAISON_TOOL = "comparisonTool"
+_COMPAISON_TOOL_KEY = "comparisonTool"
 _SETTINGS_FILE = "Comparison Settings.json"
 
 class Settings:
@@ -18,12 +20,11 @@ class Settings:
 
     def get_comparison_tool(self):
         self._ensure_comparison_tool_is_chosen()
-        return self.settings[_COMPAISON_TOOL]
+        return self.settings.get(_COMPAISON_TOOL_KEY, None)
 
     def _ensure_comparison_tool_is_chosen(self):
-        if self.settings == {}:
-            settings = load_json(_SETTINGS_FILE, default={})
-        comparisonTool = settings.get(_COMPAISON_TOOL, {})
+        self.settings = load_json(_SETTINGS_FILE, default={})
+        comparisonTool = self.settings.get(_COMPAISON_TOOL_KEY, None)
         if not comparisonTool:
             self._ask_for_comaparison_tool()
 
@@ -37,16 +38,18 @@ class Settings:
                 'Pick a comparison tool', self._get_applications_directory(),
                 self._PLATFORM_APPLICATIONS_FILTER[PLATFORM]
             )   
-        if viewer_path:
-            # viewer = get_popen_kwargs_for_opening('{file}', viewer_path)
-            self.settings[_COMPAISON_TOOL] = viewer_path
-            save_json(_SETTINGS_FILE, self.settings)    
+            if viewer_path:
+                # viewer = get_popen_kwargs_for_opening('{file}', viewer_path)
+                self.settings[_COMPAISON_TOOL_KEY] = viewer_path
+                save_json(_SETTINGS_FILE, self.settings)    
 
     def _get_applications_directory(self):
         if PLATFORM == 'Mac':
             return '/Applications'
         elif PLATFORM == 'Windows':
-            result = os.environ["ProgramFiles"]
+            result = os.environ["ProgramW6432"]
+            if not result:
+                result = os.environ["ProgramFiles"]
             if not exists(result):
                 result = splitdrive(sys.executable)[0] + '\\'
             return result
